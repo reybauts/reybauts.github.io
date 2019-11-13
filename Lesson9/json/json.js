33f551de095f2aab2409d0b33336b7a7c1a23838
reybauts <Use your GitHub email address>
2019-11-13 09:42:24 -0800

    ok

--- Lesson9/json/json.js
+++ Lesson9/json/json.js
@@ -8,7 +8,7 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
 
     const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < prophets.length; i++ ) {        
         let card = document.createElement("section");
         let alt = prophets[i].name + " " + prophets[i].lastname + " - " + i;        
         let h2 = document.createElement("h2");
@@ -16,12 +16,15 @@ fetch(requestURL)
         let birthPlace = document.createElement("p");
         let image = document.createElement("img");
         let gap = document.createElement("br");
+        let number = document.createElement("h3");
+        number.textContent = prophets[i].order;
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;       
         birthDate.textContent = "Date of birth: " + prophets[i].birthdate;
         birthPlace.textContent = "Place of birth: " + prophets[i].birthplace;                
         image.setAttribute('src', prophets[i].imageurl);
         image.setAttribute ("alt", alt);
         card.appendChild(image);
+        card.appendChild(number);
         card.appendChild(h2);
         card.appendChild(birthDate);
         card.appendChild(birthPlace);
