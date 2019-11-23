96df57cec2585b613d245eed17995d9b8183bdb8
reybauts <Use your GitHub email address>
2019-11-22 23:34:11 -0800

    ok

--- Lesson10/home/js/towns.js
+++ Lesson10/home/js/towns.js
@@ -1,8 +1,7 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 fetch(requestURL)
     .then(function (response) {
-        return response.json();
-    })
+        return response.json(); })
     .then(function (jsonObject) {
         console.table(jsonObject); // temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
